a52fe69a0b17f27383c0d18b0e301e2c2471a231
nyatinte <takumi.062757@gmail.com>
2024-05-02 00:32:24 +0900

    ignore `/` to avoid infinite redirect

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
+app.get('/', (c) => {
+  return c.html(`<h1>redirect to me</h1>`);
+});
+
 app.get('*', (c) => {
   const url = new URL(c.req.url);
   const redirectUrl = url.pathname.substring(1) + url.search;
