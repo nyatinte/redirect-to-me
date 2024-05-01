9da07a1ff29fbac5dd638439d607de106eacd462
nyatinte <takumi.062757@gmail.com>
2024-05-02 00:21:00 +0900

    implement simple redirect feature

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
+app.get('*', (c) => {
+  const url = new URL(c.req.url);
+  const redirectUrl = url.pathname.substring(1) + url.search;
+  return c.redirect(redirectUrl, 302);
 });
 
 export default app;
